eae6d70704fc5e7366396b5ebb1c7b9ddd272ddf
tuankiet091002 <tuankiet091002@gmail.com>
2023-03-25 20:26:54 +0700

    more decoration

--- src/components/Control/ManualControl/ControlSwitch/index.js
+++ src/components/Control/ManualControl/ControlSwitch/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Box, Grid, Stack, Switch, Typography, } from '@mui/material';
+import { Grid, Stack, Typography, Slider, Paper } from '@mui/material';
 
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 import ShowerIcon from '@mui/icons-material/Shower';
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import { deepPurple, lightBlue } from '@mui/material/colors';
+import { StyledSwitch } from './styles';
+
 
 const types = [
     { name: "Điều hòa", icon: <AcUnitIcon sx={{ color: lightBlue[50] }} /> },
@@ -12,23 +14,30 @@ const types = [
     { name: "Đèn", icon: <LightbulbIcon /> }
 ]
 
-const ControlSwitch = ({type}) => {
-    const {name, icon} = types[type];
+const ControlStyledSwitch = ({ type }) => {
+    const { name, icon } = types[type];
     return (
-        <Box sx={{ padding: 2, width: "10rem", height: "10rem", borderRadius: "2rem", bgcolor: deepPurple[900] }}>
-            <Grid container>
+        <Paper elevation={3} sx={{ padding: 2, width: "10rem", height: "10rem", borderRadius: "2rem", bgcolor: !type ? deepPurple[900] : null }}>
+            <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <Stack direction="row" justifyContent="space-between">
                         {icon}
-                        <Switch defaultChecked />
+                        <StyledSwitch defaultChecked />
                     </Stack>
                 </Grid>
                 <Grid item xs={12}>
-                    <Typography sx={{ color: lightBlue[50] }}>{`${name} bật`}</Typography>
+                    <Slider
+                        valueLabelDisplay
+                        defaultValue={30}
+                        sx={type ? {} : { color: 'white' }} />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Typography sx={type ? {} : { color: lightBlue[50] }}>{`${name} bật`}</Typography>
                 </Grid>
             </Grid>
-        </Box>
+        </Paper>
     )
 }
 
-export default ControlSwitch
+export default ControlStyledSwitch
--- src/components/Control/ScheduledControl/ControlCard/index.js
+++ src/components/Control/ScheduledControl/ControlCard/index.js
@@ -1,40 +1,73 @@
 import React from 'react'
-import { Stack, Card, CardHeader, CardContent, Box, Slider, Typography} from '@mui/material';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Stack, Card, CardHeader, CardContent, Box, Typography } from '@mui/material';
 import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
 import OpacityIcon from '@mui/icons-material/Opacity';
 import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
+import { red, cyan, yellow } from '@mui/material/colors';
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { TimePicker } from '@mui/x-date-pickers';
+
+import { StyledTimeField, StyledSlider, StyledStroke } from './styles';
 
 const types = [
-    { name: "Điều hòa", icon: <DeviceThermostatIcon sx={{ fontSize: 50 }} /> },
-    { name: "Máy bơm", icon: <OpacityIcon sx={{ fontSize: 50 }} /> },
-    { name: "Đèn", icon: <LightModeIcon sx={{ fontSize: 50 }} /> }
+    { name: "Điều hòa", icon: <DeviceThermostatIcon color="primary" sx={{ fontSize: "3rem" }} />, color: red[500] },
+    { name: "Máy bơm", icon: <OpacityIcon color="primary" sx={{ fontSize: "3rem" }} />, color: cyan[300] },
+    { name: "Đèn", icon: <LightModeIcon color="primary" sx={{ fontSize: "3rem" }} />, color: yellow[500] }
 ]
+
+
 const ControlCard = ({ type }) => {
-    const { name, icon } = types[type];
-    console.log(name)
+    const { name, icon, color } = types[type];
+
+    const outerTheme = createTheme({
+        palette: {
+            primary: {
+                main: color,
+            },
+        },
+    });
+
     return (
-        <Card elevation={3}>
-            <CardHeader title={name}></CardHeader>
-            <CardContent>
-                <Stack direction="row" alignItems="center" justifyContent="space-between">
-                    <TimePicker label="Bắt đầu" />
-                    <Typography sx={{ px: 1 }}>to</Typography>
-                    <TimePicker label="Kết thúc" />
-                </Stack>
-                <Stack alignItems="center" padding={3} pb={0} >
-                    <Box sx={{ maxWidth: 200 }}>
-                        <CircularProgressbarWithChildren value={40}>
-                            {icon}
-                        </CircularProgressbarWithChildren>
-                    </Box>
-                    <Slider defaultValue={50} valueLabelDisplay="auto" />
-                </Stack>
-            </CardContent>
-        </Card>
+        <ThemeProvider theme={outerTheme}>
+            <Card elevation={3}>
+                <CardHeader title={name}></CardHeader>
+                <CardContent>
+                    <Stack direction="row" alignItems="center" justifyContent="space-between">
+                        <StyledTimeField label={<Typography sx={{ display: "flex", alignItems: "center" }}>
+                            <LightModeIcon />Bắt đầu</Typography>} />
+                        <Typography sx={{ px: 1 }}>to</Typography>
+                        <StyledTimeField label={<Typography sx={{ display: "flex", alignItems: "center" }}>
+                            <DarkModeIcon />Kết thúc</Typography>} />
+                    </Stack>
+                    <Stack alignItems="center" padding={3} pb={0} >
+                        <Box sx={{ maxWidth: "10rem" }}>
+                            <CircularProgressbarWithChildren
+                                value={50}
+                                styles={{
+                                    path: {
+                                        // Path color
+                                        stroke: color,
+                                        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                                        strokeLinecap: 'butt',
+                                    }
+                                }}>
+                                {icon}
+                            </CircularProgressbarWithChildren>
+                        </Box>
+                        <Stack spacing={2} width='100%' direction="row" alignItems="center">
+                            <Typography sx={{ width: '4rem' }}>Tắt</Typography>
+                            <StyledSlider defaultValue={50} valueLabelDisplay="auto" />
+                            <Typography sx={{ width: '4rem' }}>100%</Typography>
+                        </Stack>
+
+                    </Stack>
+                </CardContent>
+            </Card>
+        </ThemeProvider>
     )
 }
 
